8a543f547c5b09ba70bf2608cb1d70198032bc94
Rasyid Rafi <muhammand.rafi25@gmail.com>
2023-07-18 12:59:33 +0700

    get API_KEY env

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,8 @@
+const apiKey = process.env.API_KEY;
+
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "193593681eeeb9f10b20ee6506e9af8b",
+  apiKey,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
